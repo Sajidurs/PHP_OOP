a59faec4108ecea039d8af2ea2cb21e1235630e3
Sajidur Rahman <shajidur171@gmail.com>
2023-03-26 02:18:26 +0600

    PHP OOP

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// PHP OOP
+class man{
+    public $name;
+    public $age;
+    public $address;
+
+    function info(){
+        return 'Sajidurs World';
+    }
+}
+
+$bappi = new man();
+$bappi->name = "Sajidur";
+echo $bappi->name;
+
+$munna = new man();
+
+
+
+
+?>
+
+
+
